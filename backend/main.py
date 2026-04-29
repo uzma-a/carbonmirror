340814ca13f23d1270310f2088e8903d6cefa64c
Uzma-Aashiya <149761349+uzma-a@users.noreply.github.com>
2026-04-29 12:55:05 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="CarbonMirror API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","http://localhost:3000","https://carbonmirror.vercel.app/"],
+    allow_origins=["http://localhost:5173","http://localhost:3000","https://carbonmirror.vercel.app/", "*"],
     allow_methods=["POST","GET"],
     allow_headers=["*"],
 )
